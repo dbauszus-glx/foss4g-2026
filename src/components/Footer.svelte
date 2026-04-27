d3798b570648b638a7e79322f986a3376ff3ead2
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2026-04-27 18:05:26 +0100

    replace FOSS4G 2026 Hiroshima

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,6 +5,6 @@
 <footer class="footer footer-horizontal footer-center bg-base-200 p-10 p-10">
   <aside>
     <p class="font-bold text-xl">{$t('title')}</p>
-    <p>Copyright © 2026 FOSS4G 2026 Hiroshima LOC</p>
+    <p>Copyright © 2026 FOSS4G 2027 Bristol LOC</p>
   </aside>
 </footer>
